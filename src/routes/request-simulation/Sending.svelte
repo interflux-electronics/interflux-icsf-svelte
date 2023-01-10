14c107e4c9bc62dd724720fb066d26a9249240b6
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-01-10 16:02:41 +1000

    Add <Sending> component for showing the submit loading state

---
+++ src/routes/request-simulation/Sending.svelte
@@ -0,0 +1,63 @@
+<script>
+	import Link from '../Link.svelte';
+	import iconDots from '$lib/images/icon-dots.svg';
+	import backgroundImage from '$lib/images/close-shot-bg.png';
+	import interfluxLogo from '$lib/images/primary-interflux-symbol-2.svg';
+</script>
+
+<section style="background-image: url({backgroundImage})">
+	<div class="content">
+		<img src={interfluxLogo} alt="logo" class="logo" />
+		<img src={iconDots} alt="" aria-hidden="true" class="icon" />
+		<h2>Sending</h2>
+		<p>One moment please.</p>
+	</div>
+</section>
+
+<style>
+	@keyframes rotate {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(360deg);
+		}
+	}
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: auto;
+		position: relative;
+		background-color: var(--green-3);
+		color: white;
+		background-size: 100% auto;
+		background-position: center top;
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 50px;
+		max-width: 280px;
+		margin-bottom: 10vw;
+	}
+	img.icon {
+		animation: rotate 500ms linear;
+		animation-name: rotate;
+		animation-duration: 3s;
+		animation-timing-function: linear;
+		animation-iteration-count: infinite;
+		animation-fill-mode: backwards;
+	}
+	.bottom-links {
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+		align-items: center;
+		margin: 5vw 0vw;
+	}
+</style>
