4756310ef1c979fca3402dc3fd8d2ef23b98d21c
Jan Werkhoven <jan.werkhoven@gmail.com>
2024-01-28 19:47:43 +1100

    Add website getters to Company model

--- src/lib/models/Company.ts
+++ src/lib/models/Company.ts
@@ -6,23 +6,36 @@ export default class Company {
     this.name = attributes['business-name'] || attributes['legal-name'];
     this.address = attributes['address'];
     this.phone = attributes['phone'];
-    this.email = attributes['email-general'] || attributes['email-orders'];
+    this.email = attributes['email-orders'] || attributes['email-general'];
     this.website = attributes['website'];
 
-    // this.countryId = relationships.find((rel) => rel.type === 'country').id;
+    this.countryId = relationships.country.data.id;
   }
 
+  // Attributes
+
+  id;
   name;
   address;
   phone;
   email;
   website;
 
-  countryId = 'AU';
+  // Relationships
+
+  countryId;
 
   // TODO: improve so that we do not need to pass in records,
   // instead we can read countries from Svelte store
   country(records) {
     return records.find((record) => record.id === this.countryId);
+  }
+
+  get websiteLink() {
+    return this.website;
+  }
+
+  get websiteLabel() {
+    return this.website.replace('https://', '').replace('http://', '').replace(/\/$/, '');
   }
 }
