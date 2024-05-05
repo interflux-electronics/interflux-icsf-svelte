9f00a18c7061d3e148f5394f4c9e3db47f0fb515
Jan Werkhoven <jan.werkhoven@gmail.com>
2024-05-05 14:34:28 +1000

    Add a general catchall 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+  import ErrorPage from '$lib/components/ErrorPage.svelte';
+
+  import { page } from '$app/stores';
+</script>
+
+<ErrorPage status={$page.status} message={$page.error?.message} />
