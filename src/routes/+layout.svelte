d2817cf025a167dfe1cd4f9ccbb654679d8f6975
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-03-06 22:01:49 +1100

    Add open graph images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
   import { inject } from '@vercel/analytics';
   import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
   import './styles.css';
+  import OgWide from '$lib/images/og-wide.jpg';
+  import OgSquare from '$lib/images/og-wide.jpg';
 
   // For visitor analytics in Vercel
   inject({ mode: dev ? 'development' : 'production' });
@@ -11,15 +13,31 @@
   // For speed analytics in Vercel
   injectSpeedInsights();
 
-  $: title = $page?.data?.seo?.title || 'Interflux ICSF Select6';
-  $: description =
-    $page?.data?.seo?.description ||
-    "World's most intelligent selective fluxer for electronics manufacturers.";
+  $: title = 'Interflux ICSF Select6';
+  $: alt = "ICSF Select6 World's most intelligent selective fluxer.";
+  $: description = "World's most intelligent selective fluxer for electronics manufacturers.";
 </script>
 
 <svelte:head>
   <title>{title}</title>
   <meta name="description" content={description} />
+
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+
+  <meta property="og:image" content={OgWide} />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
+  <meta property="og:image:alt" content={alt} />
+
+  <meta property="og:image" content={OgSquare} />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="1200" />
+  <meta property="og:image:alt" content={alt} />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:image" content={OgSquare} />
+  <meta name="twitter:image:alt" content={alt} />
 </svelte:head>
 
 <div class="app">
